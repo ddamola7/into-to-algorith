7444804f2d98837c4b7917b68df1f058ba880ccb
oyindamola <ddamola7@gmail.com>
2024-12-09 12:02:03 +0100

    intro to algorith

---
+++ index.js
@@ -0,0 +1,17 @@
+// adding threenumbers
+var num1 =17
+var num2=25
+var num3=12
+var sum= num1+num2+num3
+console.log(sum)
+
+
+// find percentage profit
+
+// selection processes
+var age = 25
+if (age > 17){
+    console.log("you can vote")
+}else{
+    console.log("you can't vote")
+}
